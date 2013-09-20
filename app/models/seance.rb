64d80f882c5e98e0f7a7a70bf1ed185455e5f8d3
d4mk0 <d4mk0f@gmail.com>
2013-09-21 00:31:29 +0400

    validation for seance duplicates was added

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -2,4 +2,10 @@ class Seance < ActiveRecord::Base
   belongs_to :hall
   
   attr_accessible :film_name, :datetime, :price, :hall
+  
+  before_create :check_double
+  
+  def check_double
+    Seance.where(:film_name => film_name, :datetime => datetime, :hall_id => hall).blank?
+  end
 end
